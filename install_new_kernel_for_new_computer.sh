2c9874835d722066a3063a4eb429720f366567a4
yingshaoxo <yingshaoxo@gmail.com>
2024-06-26 00:14:57 -0400

    update install kernel script

--- install_new_kernel_for_new_computer.sh
+++ install_new_kernel_for_new_computer.sh
@@ -1,6 +1,10 @@
+mkdir /boot_backup
+cp -fr /boot/* /boot_backup/
+
 rm -fr /boot/*
 cp -fr ./new_boot/* /boot/
 mv /boot/vmlinuz-lts /boot/vmlinuz-grsec
 mv /boot/initramfs-lts /boot/initramfs-grsec
 
 chown -R root /boot
+echo "New kernel will not let you 'mount -t vfat /dev/sdb1 /media/usb'"
